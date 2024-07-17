92921c83698f542e59d18acf5c082088e4de8b3e
Anselmo Martins <anselmomartins@institutoiracema.org.br>
2024-07-17 11:24:41 -0300

    first commit

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Serie;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class SeriesController extends Controller
+{
+    public function index()
+    {
+        $series = Serie::query()->orderBy('nome')->get();
+
+        return view('series.index')->with('series', $series);
+    }
+
+    public function create()
+    {
+        return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $name = $request->input('nome');
+        $serie = new Serie();
+        $serie->nome = $name;
+        $serie->save();
+
+        return redirect('/series');
+    }
+}
